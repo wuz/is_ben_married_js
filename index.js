a4ad56226917656dbc07a5aaa997d52d06f43d10
Conlin Durbin <conlin@hackerrank.com>
2020-07-15 15:12:58 -0400

    Make it executable?

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+#!/usr/bin/env node
 "use strict";
 
-var fetch = require("node-fetch");
+const fetch = require("node-fetch");
 
-function isBenMarried() {
-  return fetch("https://isbenmarriedyet.com/api.json")
+const isBenMarried = () =>
+  fetch("https://isbenmarriedyet.com/api.json")
     .then((res) => res.json())
     .then(({ married }) => (married ? "Yes" : "No"));
-}
 
-function main() {
-  isBenMarried().then(console.log);
-}
+const main = async () => {
+  console.log(await isBenMarried());
+};
 
 main();
